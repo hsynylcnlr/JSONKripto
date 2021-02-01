015a9ebf5be87311443446179c0d4383ee093e19
Hüseyin Yalçınlar <huseyinyalcinlar@icloud.com>
2021-02-01 20:44:09 +0300

    Initial Commit

---
+++ Kripto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kripto
+//
+//  Created by Hüseyin Yalçınlar on 1.02.2021.
+//  Copyright © 2021 Hüseyin Yalçınlar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
